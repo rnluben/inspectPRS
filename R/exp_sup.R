262200b25828b828fb5a01ca1ce2759358ed6473
Robert Luben <r.luben@ucl.ac.uk>
2024-04-26 16:24:21 +0100

    Times character now should in hex format

--- R/exp_sup.R
+++ R/exp_sup.R
@@ -3,6 +3,6 @@ exp_sup <- function(x, pvalue = 2, boldsig=FALSE, md=FALSE, sig=0.05) {
    SupEnd <- ifelse(md,"^","</sup>")
    BoldStart <- ifelse(md,"**","<b>")
    BoldEnd <- ifelse(md,"**","</b>")
-   p_formatted <- ifelse(is.na(x),"",sprintf(paste0("%4.", pvalue, "f × 10",SupStart,"%d",SupEnd), x / 10^floor(log10(abs(x))), floor(log10(abs(x)))))
+   p_formatted <- ifelse(is.na(x),"",sprintf(paste0("%4.", pvalue, "f \U00D7 10",SupStart,"%d",SupEnd), x / 10^floor(log10(abs(x))), floor(log10(abs(x)))))
    ifelse(x < sig &  boldsig, paste0(BoldStart,p_formatted,BoldEnd),p_formatted)
 }
