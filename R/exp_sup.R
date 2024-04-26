0e4263060230dd79a47428beafff0f375d602187
Robert Luben <robert.luben@gmail.com>
2024-04-26 16:10:23 +0100

    Included md argument to toggle between markdown and HTML

---
+++ R/exp_sup.R
@@ -0,0 +1,8 @@
+exp_sup <- function(x, pvalue = 2, boldsig=FALSE, md=FALSE, sig=0.05) {
+   SupStart <-ifelse(md,"^","<sup>")
+   SupEnd <- ifelse(md,"^","</sup>")
+   BoldStart <- ifelse(md,"**","<b>")
+   BoldEnd <- ifelse(md,"**","</b>")
+   p_formatted <- ifelse(is.na(x),"",sprintf(paste0("%4.", pvalue, "f × 10",SupStart,"%d",SupEnd), x / 10^floor(log10(abs(x))), floor(log10(abs(x)))))
+   ifelse(x < sig &  boldsig, paste0(BoldStart,p_formatted,BoldEnd),p_formatted)
+}
